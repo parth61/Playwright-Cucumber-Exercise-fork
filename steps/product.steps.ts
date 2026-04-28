aad70d3d1d146aeb9cb7e7473f659772969d1df8
Parth Ramani <129743635+parth61@users.noreply.github.com>
2026-04-28 20:46:24 +0530

    Add sorting functionality for product items

--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -1,7 +1,22 @@
 import { Then } from '@cucumber/cucumber';
 import { getPage } from '../playwrightUtilities';
 import { Product } from '../pages/product.page';
+import { expect } from '@playwright/test';
 
 Then('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
+});
+
+Then('I sort items by {string}', async (sortOption) => {
+  await new Product(getPage()).sortBy(sortOption);
+});
+
+Then('products should be sorted {string}', async (order) => {
+  const prices = await new Product(getPage()).getAllPrices();
+
+  const sorted = [...prices].sort((a, b) =>
+    order === 'ascending' ? a - b : b - a
+  );
+
+  expect(prices).toEqual(sorted);
 });
